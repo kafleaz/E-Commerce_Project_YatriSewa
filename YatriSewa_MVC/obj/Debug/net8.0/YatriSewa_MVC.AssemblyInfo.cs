05f21f8e438ec1f54cc2e160614cb1bb09d71e0e
kafleaz <rajish126@gmail.com>
2024-05-09 19:59:13 +0545

    Merged with kosang

--- YatriSewa_MVC/obj/Debug/net8.0/YatriSewa_MVC.AssemblyInfo.cs
+++ YatriSewa_MVC/obj/Debug/net8.0/YatriSewa_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YatriSewa_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b185d59969c74edbcd11257589d53cfdd2e567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3e3d31caad428b3eaf5d86c5ffd251611f814d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YatriSewa_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YatriSewa_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
